fb258568a7ce32d26d0e210fe9296645c04e7023
nicolehandel1 <nicolehandel@hbhtherapy.com>
2022-02-01 00:19:23 -0500

    acf json commit

--- themes/hbhtherapy/functions.php
+++ themes/hbhtherapy/functions.php
@@ -210,3 +210,30 @@ function enqueue_load_fa() {
 wp_enqueue_style( 'load-fa', 'https://use.fontawesome.com/releases/v5.0.13/css/all.css' );
 }
 
+/**
+ * ACF Fields
+ */
+
+define( 'MY_PLUGIN_DIR_PATH', untrailingslashit( plugin_dir_path( __FILE__ ) ) );add_filter('acf/settings/save_json', 'my_acf_json_save_point');
+ 
+function my_acf_json_save_point( $path ) {
+    
+    // Update path
+    $path = MY_PLUGIN_DIR_PATH. '/acf-json';
+    // Return path
+    return $path;
+    
+}
+
+add_filter('acf/settings/load_json', 'my_acf_json_load_point');
+
+/**
+ * Register the path to load the ACF json files so that they are version controlled.
+ * @param $paths The default relative path to the folder where ACF saves the files.
+ * @return string The new relative path to the folder where we are saving the files.
+ */
+function my_acf_json_load_point( $paths ) {
+   // Remove original path
+   unset( $paths[0] );// Append our new path
+   $paths[] = MY_PLUGIN_DIR_PATH. '/acf-json';   return $paths;
+}
