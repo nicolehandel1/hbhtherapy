7cc55a43188caa63ca4a139500074e67843c151e
nicolehandel1 <nicolehandel@hbhtherapy.com>
2022-03-31 11:52:17 -0400

    blog link

--- themes/hbhtherapy/post-template-parts/blog/component-info.php
+++ themes/hbhtherapy/post-template-parts/blog/component-info.php
@@ -85,7 +85,7 @@
             <hr>
             <a class="phone" href="tel:<?php the_field( 'header_phone_number_link', 'option' ); ?>"><?php the_field( 'header_phone_number', 'option' ); ?></a>
 
-            <a class="btn" href="" target="_blank"><?php the_field( 'clinician_button_label', 'option' ); ?></a>
+            <a class="btn" href="<?php the_field( 'view_availability_link', 'option' ); ?>" target="_blank"><?php the_field( 'clinician_button_label', 'option' ); ?></a>
             
             <a class="archive-link" href="/counselors-appointments/"><?php the_field( 'clinicians_view_all_label', 'option' ); ?></a>
 
