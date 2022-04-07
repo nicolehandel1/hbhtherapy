436b7d6020cbc1779a96aaf27604761fc73fda59
nicolehandel1 <nicolehandel@hbhtherapy.com>
2022-04-07 16:08:59 -0400

    commit

--- themes/hbhtherapy/page-template-parts/about/component-covid.php
+++ themes/hbhtherapy/page-template-parts/about/component-covid.php
@@ -1,6 +1,6 @@
 <div class="section covid-section abt-section" id="covid">
     <div class="section-content">
-        <h1 class="pg-title abt-title">Covid Response</h1>
+        <h1 class="pg-title abt-title covid-title">Covid Response</h1>
         <p class="srvpg-summary abt-sum covid-sum"><?php the_field( 'about_hero_text' ); ?></p>
     </div>
 </div>
