7bc83b9e893197062825f990d1d56bc9b7201812
nicolehandel1 <nicolehandel@hbhtherapy.com>
2022-04-25 21:26:02 -0400

    commit cr

--- themes/hbhtherapy/page-template-parts/about/component-covid.php
+++ themes/hbhtherapy/page-template-parts/about/component-covid.php
@@ -1,6 +1,10 @@
 <div class="section covid-section abt-section" id="covid">
-    <div class="section-content">
+    <div class="section-content covid-content">
         <h1 class="pg-title abt-title covid-title"><?php the_field( 'covid_section_title' ); ?></h1>
         <p class="abt-sum covid-sum"><?php the_field( 'covid_section_content' ); ?></p>
+        
+        <?php $covid_button_link = get_field( 'covid_button_link' ); if ( $covid_button_link ) : ?>
+        <div class="flex-center"><a class="btn" href="<?php echo esc_url( $covid_button_link); ?>"><?php the_field( 'covid_button_label' ); ?></a></div>
+        <?php endif; ?>
     </div>
 </div>
