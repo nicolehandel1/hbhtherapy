df877c4f64d63ca4774750b3e8e425122654af53
nicolehandel1 <nicolehandel@hbhtherapy.com>
2022-05-04 13:58:57 -0400

    commit target sitemap

--- themes/hbhtherapy/footer.php
+++ themes/hbhtherapy/footer.php
@@ -255,7 +255,7 @@
                 <ul>
                     <?php $privacy_page_link = get_field( 'privacy_page_link', 'option' ); ?>
                     <li><a href="<?php echo esc_url( $privacy_page_link); ?>">Privacy Policy & Terms of Use</a></li>
-                    <li><a href="<?php the_field( 'sitemap_link', 'option' ); ?>">Sitemap</a></li>
+                    <li><a href="<?php the_field( 'sitemap_link', 'option' ); ?>" target="_blank">Sitemap</a></li>
                     <li>&copy; <?php echo date("Y"); ?> Handel Behvioral Health</li>
                 </ul> 
 
