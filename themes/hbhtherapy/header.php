04e2a617d39749ad3ec4f494cebb71b4a211f2b5
nicolehandel1 <nicolehandel@hbhtherapy.com>
2022-05-04 20:16:13 -0400

    commit

--- themes/hbhtherapy/header.php
+++ themes/hbhtherapy/header.php
@@ -14,6 +14,14 @@
 <html <?php language_attributes(); ?>>
 
 <head>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-NQ4TKDH');</script>
+    <!-- End Google Tag Manager -->
+    
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="https://gmpg.org/xfn/11">
@@ -22,6 +30,7 @@
     <meta property="og:title" content="<?php the_title() ?>"/>
     <meta property="og:image" content="<?php the_post_thumbnail() ?>"/>
     <meta property="og:url" content="<?php the_permalink(); ?>"/>
+    
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=AW-CONVERSION_ID"></script>
     <script>
@@ -37,6 +46,11 @@
 </head>
 
     <body <?php body_class(); ?>>
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NQ4TKDH"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
+        
     <?php wp_body_open(); ?>
     <div id="page" class="site">
         <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'hbhtherapy' ); ?></a>
