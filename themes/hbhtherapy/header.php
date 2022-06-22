c4571f7076385c515e61987871a70f39be08bf9a
nicolehandel1 <nicolehandel@hbhtherapy.com>
2022-06-22 14:34:54 -0400

    ss media code commit

--- themes/hbhtherapy/header.php
+++ themes/hbhtherapy/header.php
@@ -40,8 +40,26 @@
     gtag('js', new Date());
     gtag('config', 'AW-CONVERSION_ID');
     gtag('config', 'UA-227614035-1');
-
     </script>
+    
+    <!-- Meta Pixel Code -->
+    <script>
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '394515039158653');
+    fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+    src="https://www.facebook.com/tr?id=394515039158653&ev=PageView&noscript=1"
+    /></noscript>
+    <!-- End Meta Pixel Code -->
+
     <?php wp_head(); ?>
 </head>
 
