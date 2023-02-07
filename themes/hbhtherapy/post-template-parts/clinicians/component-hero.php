453135f3a5ad1222b9baff09da99a283c0c0b893
nicolehandel1 <nicolehandel@hbhtherapy.com>
2023-02-07 12:53:31 -0500

    telehealth commit

--- themes/hbhtherapy/post-template-parts/clinicians/component-hero.php
+++ themes/hbhtherapy/post-template-parts/clinicians/component-hero.php
@@ -1,3 +1,8 @@
+<style>
+a.btn {
+    font-size: 18px;
+}
+</style>
 <?php 
 
 $location = wp_get_post_terms($post->ID, 'clinician-location');
@@ -14,7 +19,7 @@ $license = wp_get_post_terms($post->ID, 'clinician-licensure');
         </div>
 
         <div class="single-hero-info">
-
+            <a class="archive-link" href="/counselors-appointments/">Counselors & Appointments</a>
             <h1 class="blog-title"><?php the_title( ); ?>,
 
                 <?php if ($license) { $out = array(); foreach ($license as $license) {
@@ -30,6 +35,12 @@ $license = wp_get_post_terms($post->ID, 'clinician-licensure');
             <a class="phone" href="tel:<?php the_field( 'number_link' ); ?>"><?php the_field( 'number_label' ); ?></a>
 
             <a class="btn <?php $terms = wp_get_post_terms(get_the_id(), 'clinician-color');  foreach ($terms as $term) { echo $term->slug.' ';} ?>" href="<?php the_field( 'clinician-book_now_link' ); ?>" target="_blank"><?php the_field( 'clinician_button_label', 'option' ); ?></a>
+            
+            <?php if(get_field('telehealth_link') == "Clinician will send"): ?>
+                <?php // do nothing ?>
+           <?php elseif( get_field('telehealth_link') ): ?>
+                <a class="btn" href="<?php the_field( 'telehealth_link' ); ?>" target="_blank">Join Virtual Waiting Room</a>
+            <?php else: endif;?>
 
         </div>
 
