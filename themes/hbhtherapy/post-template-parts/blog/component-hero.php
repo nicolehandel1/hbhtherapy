03003ac3700fc917b71346f5877ee2720964f887
nicolehandel1 <nicolehandel@hbhtherapy.com>
2026-03-10 20:10:39 -0400

    blog heading commit

--- themes/hbhtherapy/post-template-parts/blog/component-hero.php
+++ themes/hbhtherapy/post-template-parts/blog/component-hero.php
@@ -17,7 +17,11 @@ $alt_text = get_post_meta($imageID , '_wp_attachment_image_alt', true);
             
             <a class="archive-link" href="/mental-health-blog/">Mental Health Blog</a>
 
-            <h1 class="blog-title"><?php the_title(); ?></h1>
+            <?php if ( get_field('svc-heading') ) : ?>
+                <h1 class="blog-title"><?php the_field('blog-heading'); ?></h1>
+            <?php else : ?>
+                <h1 class="blog-title"><?php the_title(); ?></h1>
+            <?php endif; ?>
 
             <?php if( get_field('blog_subtitle') ): ?>
             <h2 class="blog-hero-subtitle"><?php the_field( 'blog_subtitle' ); ?></h2>
