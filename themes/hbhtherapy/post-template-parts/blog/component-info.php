5ea3b6b18cfa4b7305536833b7892a6c5fb0a7cc
nicolehandel1 <nicolehandel@hbhtherapy.com>
2026-03-10 23:01:48 -0400

    blog html commit

--- themes/hbhtherapy/post-template-parts/blog/component-info.php
+++ themes/hbhtherapy/post-template-parts/blog/component-info.php
@@ -1,26 +1,26 @@
 <div class="section blg-info-section">
-<div class="section-content blog-info-contnt">
-<div class="single-content">
+    <div class="section-content blog-info-contnt">
 
-<?php if (have_rows('blog_content')) :
-while (have_rows('blog_content')) : the_row();
+        <div class="single-content">
 
-switch (get_row_layout()) :
+            <?php if (have_rows('blog_content')):
+while (have_rows('blog_content')): the_row();
+
+switch (get_row_layout()):
 
 case 'section_title': ?>
-<h2 class="blog-subtitle"><?php the_sub_field('blog_section_title'); ?></h2>
-<?php break;
+            <h2 class="blog-subtitle"><?php the_sub_field('blog_section_title'); ?></h2>
+            <?php break;
 
 case 'section_content': ?>
-<p><?php the_sub_field('blog_section_content'); ?></p>
-<?php break;
+            <p><?php the_sub_field('blog_section_content'); ?></p>
+            <?php break;
 
 case 'image':
 $img = get_sub_field('blog_content_image');
-if ($img) : ?>
-<img src="<?= esc_url($img['url']); ?>" alt="<?= esc_attr($img['alt']); ?>">
-<?php endif;
-break;
+if ($img): ?>
+            <img src="<?=esc_url($img['url']);?>" alt="<?=esc_attr($img['alt']);?>">
+            <?php endif; break;
 
 case 'quote':
 the_sub_field('quote');
@@ -32,106 +32,102 @@ endwhile;
 endif;
 ?>
 
-<!-- Author -->
-<?php if (have_rows('blog_author')) : ?>
+            <?php if (have_rows('blog_author')): ?>
+            <h4 class="blog-subtitle bauthor-title">About The Author</h4>
 
-<h4 class="blog-subtitle bauthor-title">About The Author</h4>
+            <?php while (have_rows('blog_author')): the_row();
 
-<?php while (have_rows('blog_author')) : the_row();
+$post = get_sub_field('blog_author_other') ?: get_sub_field('blog_authot_clincician');
 
-if (get_row_layout() == 'other') :
+if ($post): setup_postdata($post); ?>
 
-$post = get_sub_field('blog_author_other');
+            <div class="bauthor-wrap">
 
-elseif (get_row_layout() == 'clinician') :
+                <?php if (get_row_layout()=='other'): ?>
 
-$post = get_sub_field('blog_authot_clincician');
+                <img class="bauthor-img" src="<?php the_field('author_bioheadshot');?>" alt="<?php the_field('author_bioname');?> Headshot">
 
-endif;
+                <div>
+                    <h4 class="bauthor-name"><?php the_field('author_bioname');?></h4>
+                    <p class="bauthor-bio"><?php the_field('author_bio');?></p>
+                </div>
 
-if ($post) : setup_postdata($post); ?>
+                <?php else: ?>
 
-<div class="bauthor-wrap">
+                <img class="bauthor-img" src="<?php the_field('clinician-headshot');?>" alt="<?php the_title();?> Headshot">
 
-<?php if (get_row_layout() == 'other') : ?>
-
-<img class="bauthor-img" src="<?php the_field('author_bioheadshot'); ?>" data-rjs="2" alt="<?php the_field('author_bioname'); ?> Headshot">
-<div>
-<h4 class="bauthor-name" rel="author"><?php the_field('author_bioname'); ?></h4>
-<p class="bauthor-bio"><?php the_field('author_bio'); ?></p>
-</div>
-
-<?php else : ?>
-
-<img class="bauthor-img" src="<?php the_field('clinician-headshot'); ?>" data-rjs="2" alt="<?php the_title(); ?> Headshot">
-<div>
-<h4 class="bauthor-name" rel="author">
-<?php
+                <div>
+                    <h4 class="bauthor-name">
+                        <?php
 the_title();
-$license = wp_get_post_terms($post->ID,'clinician-licensure');
-if ($license) echo ', '.join(', ', wp_list_pluck($license,'name'));
+$terms = wp_get_post_terms($post->ID,'clinician-licensure');
+if($terms) echo ', '.join(', ', wp_list_pluck($terms,'name'));
 ?>
-</h4>
+                    </h4>
 
-<p class="bauthor-bio">
-<?php the_field('author_summary'); ?>
-<a href="<?php the_permalink(); ?>" class="bauthor-more">More About Author →</a>
-</p>
+                    <p class="bauthor-bio">
+                        <?php the_field('author_summary');?>
+                        <a href="<?php the_permalink();?>" class="bauthor-more">More About Author →</a>
+                    </p>
+                </div>
 
-</div>
+                <?php endif; ?>
 
-<?php endif; ?>
+            </div>
 
-</div>
-
-<?php wp_reset_postdata();
+            <?php wp_reset_postdata();
 endif;
-
 endwhile;
-endif;
-?>
+endif; ?>
 
-</div>
+        </div>
 
-<div class="sidebar">
 
-<p class="clinician-subtitle">Share</p>
-<hr>
+        <aside class="sidebar">
 
-<div class="share-icons">
-<a href="http://www.facebook.com/sharer.php?u=<?php the_permalink(); ?>" target="_blank"><i class="fab fa-facebook-f"></i></a>
-<a href="https://twitter.com/share?url=<?php the_permalink(); ?>" target="_blank"><i class="fab fa-twitter"></i></a>
-<a href="http://www.linkedin.com/shareArticle?mini=true&amp;url=<?php the_permalink(); ?>" target="_blank"><i class="fab fa-linkedin-in"></i></a>
-</div>
+            <section>
+                <p class="clinician-subtitle">Share</p>
+                <hr>
+                <div class="share-icons">
+                    <a href="http://www.facebook.com/sharer.php?u=<?php the_permalink();?>" target="_blank"><i class="fab fa-facebook-f"></i></a>
+                    <a href="https://twitter.com/share?url=<?php the_permalink();?>" target="_blank"><i class="fab fa-twitter"></i></a>
+                    <a href="http://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink();?>" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+            </section>
 
-<p class="clinician-subtitle">Subscribe To Our Newsletter</p>
-<hr>
-<button class="btn" onclick="show('popup')">Subscribe</button>
+            <section>
+                <p class="clinician-subtitle">Subscribe To Our Newsletter</p>
+                <hr>
+                <button class="btn" onclick="show('popup')">Subscribe</button>
 
-<div class="popup" id="popup">
-<a class="close" href="#" onclick="hide('popup')">x</a>
-<p class="clinician-subtitle" style="padding-left:30px;">Subscribe To Our Newsletter</p>
-<hr>
-<?php echo mailster_form(1); ?>
-</div>
+                <div id="popup" class="popup">
+                    <a class="close" onclick="hide('popup')">x</a>
+                    <p class="clinician-subtitle">Subscribe To Our Newsletter</p>
+                    <hr>
+                    <?php echo mailster_form(1); ?>
+                </div>
+            </section>
 
-<p class="clinician-subtitle">Appointments</p>
-<hr>
+            <section>
+                <p class="clinician-subtitle">Appointments</p>
+                <hr>
 
-<a class="phone" href="tel:<?php the_field('header_phone_number_link','option'); ?>">
-<?php the_field('header_phone_number','option'); ?>
-</a>
+                <a class="phone" href="tel:<?php the_field('header_phone_number_link','option');?>">
+                    <?php the_field('header_phone_number','option');?>
+                </a>
 
-<a class="btn" href="<?php the_field('view_availability_link','option'); ?>" target="_blank">
-<?php the_field('clinician_button_label','option'); ?>
-</a>
+                <a class="btn" href="<?php the_field('view_availability_link','option');?>" target="_blank">
+                    <?php the_field('clinician_button_label','option');?>
+                </a>
+            </section>
 
-</div>
-</div>
+        </aside>
+
+    </div>
 </div>
 
 <script>
-const $=id=>document.getElementById(id);
-const show=id=>$(id).style.display='block';
-const hide=id=>$(id).style.display='none';
+    const $ = id => document.getElementById(id);
+    const show = id => $(id).style.display = 'block';
+    const hide = id => $(id).style.display = 'none';
 </script>
