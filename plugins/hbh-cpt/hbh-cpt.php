8c72d4ebf9e9ab5b037091626c8e776031fb2cb8
nicolehandel1 <nicolehandel@hbhtherapy.com>
2026-04-15 12:46:56 -0400

    Update hbh-cpt.php

--- plugins/hbh-cpt/hbh-cpt.php
+++ plugins/hbh-cpt/hbh-cpt.php
@@ -159,6 +159,19 @@ function clinician_taxonomy() {
             )
         )
     );
+    register_taxonomy(
+        'clinician-language',  // The name of the taxonomy. Name should be in slug form (must not contain capital letters or spaces).
+        'clinician',             // post type name
+        array(
+            'hierarchical' => true,
+            'label' => 'Language', // display name
+            'query_var' => true,
+            'rewrite' => array(
+                'slug' => 'clinician-language',    // This controls the base slug that will display before each term
+                'with_front' => false  // Don't display the category base before
+            )
+        )
+    );
 }
 add_action( 'init', 'clinician_taxonomy');
 
